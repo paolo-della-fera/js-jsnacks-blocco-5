7ff021801e4b7a4e2ae3eb7dbcd874c0da4f19c8
paolo-della-fera <paolodf2005@gmail.com>
2026-02-20 15:57:14 +0100

    complete snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -20,4 +20,8 @@ const students = [
 ];
 
 // Recupera i dati dello studente con id 2
+const studentTwo = students.find(student => student.id === 2);
+
+console.log(studentTwo)
+
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
